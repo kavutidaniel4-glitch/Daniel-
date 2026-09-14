39acaba475b3254b590ff7232d057875492ddd3a
kavutidaniel4-glitch <kavutidaniel4@gmail.com>
2026-09-14 13:30:26 +0300

    Add files via upload

---
+++ Danie x.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+//Name  :Daniel kavuti 
+//Reg No:CT100/G/30715/26
+int main(){
+printf ("UNITS IN THIS SEMESTER\n");
+printf("1.Introduction to programming and algorithms\n");
+printf ("2.Analogue electronics\n");
+printf ("3.Mathematics for science\n");
+printf ("4.Introduction to computer applications\n");
+printf ("5.Fundamentals of digital and artificial intelligence\n");
+printf ("6.Essentials of accounting\n");
+return 0;
+}
